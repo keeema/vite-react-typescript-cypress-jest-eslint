b5d5ed67227863cf7d1b1109fe6173c0434576e5
Tomáš Růt <trut.cz@gmail.cz>
2022-02-05 21:12:31 +0100

    Update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import path from "path";
-import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [legacy(), reactRefresh()],
   esbuild: {
     jsxInject: `import React from 'react'`, // automatically import React in jsx files
   },
   resolve: {
-    alias: { 
-      "@/" : `${path.resolve(__dirname, "src")}/` 
+    alias: {
+      // for TypeScript path alias import like : @/x/y/z
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
 });
